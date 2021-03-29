843b5e061b52abac8e2641699114d3d741d2a6a1
DarioDalte <dariodalte@gmail.com>
2021-03-29 19:44:58 +0200

    frame funzionante

--- src/Test_Frame.java
+++ src/Test_Frame.java
@@ -2,12 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Test_Frame extends JFrame{
-    private static JFrame frame;
+
 
     public Test_Frame() {
-        this.frame = new JFrame();
-        this.frame.setDefaultCloseOperation(3);
-        this.frame.setSize(800,800);
-        this.frame.setLayout(new GridLayout());
+        this.setDefaultCloseOperation(3);
+        this.setSize(200,200);
+        this.setLayout(new GridLayout());
     }
 }
