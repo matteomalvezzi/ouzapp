afe4e154bb0505d5b927b6cf70516105ac5b2795
DarioDalte <dariodalte@gmail.com>
2021-03-31 10:13:10 +0200

    Migliorato un po' il codice

--- src/Fake_putty.java
+++ src/Fake_putty.java
@@ -7,24 +7,39 @@ public class Fake_putty{
     public static final Dimension MAIN_FRAME_DIMENSION = new Dimension(600, 450);
 
     /** private Attributes **/
-    private static GUI obj;// obj will be used for our second thread
+    private static GUI user_interface;// obj will be used for our second thread
 
     /** public methods **/
 
-    /**
-      * Create new frame
-      *
-      * @param user_interface
-      * @return JFrame object
-      *
-      **/
-    public static JFrame new_frame(GUI user_interface){
 
+    /** Centre the frame
+     *
+     * @param frame
+     *
+     * **/
+    public static void centreFrame(Window frame) {
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
+        frame.setLocation(x, y);
+    }
+
+
+    /** Create new frame **/
+    public static void new_frame(){
+
+        user_interface = new GUI();
         JFrame frame= new JFrame();
-        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(Fake_putty.MAIN_FRAME_DIMENSION);
+        ImageIcon foto = new ImageIcon("fake_putty.png");
+        frame.setIconImage(foto.getImage());
+        frame.setTitle("Fake Putty");
+        frame.setResizable(false);
+        frame.setAlwaysOnTop(true);
         frame.setContentPane(user_interface.getPannello_main());
-
-        return frame;
+        centreFrame(frame);
+        frame.setVisible(true);
     }
 
     /** main method **/
@@ -38,27 +53,20 @@ public class Fake_putty{
                 }
             }
 
-            obj = new GUI();
-            JFrame frame = new_frame(obj);
-            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            ImageIcon foto = new ImageIcon("Fake_Putty.png");
-            frame.setIconImage(foto.getImage());
-            frame.setSize(Fake_putty.MAIN_FRAME_DIMENSION);
-            frame.setTitle("Fake_Putty");
-            frame.setAlwaysOnTop(true);
+            new_frame();
 
         } catch (IllegalAccessException | UnsupportedLookAndFeelException | InstantiationException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
- /**start listening thread, this thread will control if any change is committed on our variable data_recive **/
+    /**start listening thread, this thread will control if any change is committed on our variable data_recive **/
     public static void startSecondThreadClient(){
-        Thread t1 = new Thread(obj);
+        Thread t1 = new Thread(user_interface);
         t1.start();
     }
 
     public static void startSecondThreadServer(){
-        Thread t1 = new Thread(obj);
+        Thread t1 = new Thread(user_interface);
         t1.start();
     }
 }
--- src/GUI.java
+++ src/GUI.java
@@ -11,7 +11,7 @@ import java.net.UnknownHostException;
 
 public class GUI implements Runnable{
 
-    /** private Attribute **/
+    /** private Attributes **/
     private JPanel pannello_main;
     private JTabbedPane selezione;
     private JButton inviaButton;
@@ -20,7 +20,6 @@ public class GUI implements Runnable{
     private JTextField ip_serverTextField;
     private JSpinner porta_server;
     private JSpinner porta_client;
-
     private JTextField ip_clientTextField;
     private JTextArea errori;
     private JTextField contenutoMessaggio;
@@ -28,6 +27,7 @@ public class GUI implements Runnable{
     private Client client;
     private Server server;
     private int codiceErrore;
+
 
     /** method to set gui style **/
     public void set_Gui_style(){
@@ -37,10 +37,13 @@ public class GUI implements Runnable{
         this.errori.setEditable(false);
         this.ip_serverTextField.setEditable(false);
         this.ip_serverTextField.setText(get_ip_host());
-
     }
 
-    /** this method will write any message on the chatBox areaMessaggi **/
+    /** this method will write any message on the chatBox areaMessaggi
+     *
+     * @param message  ----> that will be write on the chatBox
+     *
+     * **/
     public void write_on_areaMessaggi(String message){
         areaMessaggi.setText(areaMessaggi.getText() + "\n" + message);
     }
@@ -94,7 +97,6 @@ public class GUI implements Runnable{
             public void keyPressed(KeyEvent e) {
                 int evento = e.getKeyCode();
                 if (evento == KeyEvent.VK_ENTER) {
-                    System.out.println("enter: " + evento);
                     if (!contenutoMessaggio.getText().isEmpty()) {
                         write_on_areaMessaggi(contenutoMessaggio.getText());
                         if (isClient()){
@@ -114,7 +116,6 @@ public class GUI implements Runnable{
 
     /** get ip host method**/
     public String get_ip_host(){
-        //get host ip
         try {
             return Inet4Address.getLocalHost().getHostAddress();
         } catch (UnknownHostException e) {
@@ -135,65 +136,75 @@ public class GUI implements Runnable{
         this.errori.setText(information);
     }
 
-    /** onClick procediButton method
-     *  Start_message
-     * **/
+
+
+
+    /** Method that will start program as a client **/
+    public void start_client(){
+        System.out.println("Provo a connettermi...");
+        set_information("Provo a connettermi...\n");
+        client = new Client((Integer) porta_client.getValue(), ip_clientTextField.getText());
+        if (client.error_code == 1){
+            /** the pc becomes the client **/
+            set_information("---CONNESSIONE STABILITA---\n");
+            Thread tClient = new Thread(client);
+            tClient.start();
+
+            client.send_data(nickname.getText() + " partecipa\n");
+
+            Fake_putty.startSecondThreadClient();
+
+            System.out.println("GUI: " + client.data_recive);
+
+        }else if (client.error_code == 2){
+            set_error("Errore, host sconosciuto!\n");
+        }else if (client.error_code == 3){
+            set_error("Errore, impossibile stabilire la connessione!\n");
+        }
+    }
+
+
+
+
+    /** Method that will start program as a server **/
+    public void start_server(){
+        set_information("Server in attesa di un client...");
+        server = new Server((Integer) porta_server.getValue(), nickname.getText());
+
+        if (server.error_code == 1) {
+            Thread tServer = new Thread(server); // we're using threads to listen and write any change
+            tServer.start();
+
+            server.send_data(nickname.getText() + " partecipa\n");
+            set_information("connessione avvenuta correttamente...");
+
+
+            Fake_putty.startSecondThreadServer();
+
+        } else if (server.error_code == 2) {
+            set_error("errore, qualcosa è andato storto, nessun client si e' connesso");
+        }
+    }
+
+
+
+    /** onClick procediButton method Start_message**/
     public void start_message(){
         if(nickname.getText().isEmpty()){
             set_error("Errore, inserisci il nickname!");
         }
         else {
             if (isClient()) {
-                set_information("Provo a connettermi...\n");
-
-                client = new Client((Integer) porta_client.getValue(), ip_clientTextField.getText());
-
-                if (client.error_code == 1){
-                    /** the pc becomes the client **/
-                    set_information("---CONNESSIONE STABILITA---\n");
-                    client.send_data(nickname.getText() + " partecipa\n");
-
-                    Thread tClient = new Thread(client);
-                    tClient.start();
-                    Fake_putty.startSecondThreadClient();
-
-                    System.out.println("GUI: " + client.data_recive);
-
-                }else if (client.error_code == 2){
-                    set_error("Errore, host sconosciuto!\n");
-                }else if (client.error_code == 3){
-                    set_error("Errore, impossibile stabilire la connessione!\n");
-                }
-
+                start_client();
             } else if (isServer()) {
                 /** the pc becomes the server **/
-                    System.out.println("qui entro......");
-                    set_information("Server in attesa di un client...");
-
-                    server = new Server((Integer) porta_server.getValue(), nickname.getText());
-
-                    if (server.error_code == 1) {
-                        server.send_data(nickname.getText() + " partecipa\n");
-                        set_information("connessione avvenuta correttamente...");
-
-
-                        Thread tServer = new Thread(server); // we're using threads to listen and write any change
-                        tServer.start();
-                        Fake_putty.startSecondThreadServer();
-
-                    } else if (server.error_code == 2) {
-                        set_error("errore, qualcosa è andato storto, nessun client si e' connesso");
-                    }
+                    start_server();
             } else {
                 set_error("Errore, inserisci i parametri correttemente!");
             }
         }
     }
 
-    /** public Constructor number 2 **/
-    public GUI(String testo){
-        areaMessaggi.setText(areaMessaggi.getText() + testo + "\n");
-    }
 
     /** public Constructor **/
     public GUI() {
@@ -203,51 +214,61 @@ public class GUI implements Runnable{
         set_Listener();
 
     }
+
+
+    /** client is listening for receiving message **/
+    public void client_is_listening(){
+        String recived_data = client.data_recive;
+        while (true){
+            try {
+                System.out.println("dato dopo " + client.data_recive);
+                if(!client.data_recive.equals(recived_data)){
+                    System.out.println("dentro   " + recived_data);
+                    recived_data = client.data_recive;
+                    areaMessaggi.setText(areaMessaggi.getText() + "\n" + recived_data);
+                }
+            }
+            catch (NullPointerException e) { e.printStackTrace(); }
+        }
+    }
+
+
+    /** server is listening for receiving message **/
+    public void server_is_listening(){
+        String recived_data = server.data_recive;
+
+        while (true){
+            try{
+                System.out.println("dato dopo " + server.data_recive);
+                if(!server.data_recive.equals(recived_data)){
+                    System.out.println("dentro   " + recived_data);
+                    recived_data = server.data_recive;
+                    areaMessaggi.setText(areaMessaggi.getText() + "\n" + recived_data);
+                }
+            }
+            catch (NullPointerException e) { e.printStackTrace(); }
+        }
+    }
     /** This method will be executed by the second thread **/
     public void run() {
 
+
         if (isClient()){
-            /**in case the pc acts as a client this function will listen to anything the server will write, if something is added, this will write it on the chatbox**/
-            String recived_data = client.data_recive;
+            /** in case the pc acts as a client this function will listen to anything the server will write,
+             * if something is added, this will write it on the chatbox**/
+            client_is_listening();
 
-            while (true){
-
-                try {
-                    System.out.println("dato dopo " + client.data_recive);
-                    if(!client.data_recive.equals(recived_data)){
-                        System.out.println("dentro   " + recived_data);
-                        recived_data = client.data_recive;
-                        areaMessaggi.setText(areaMessaggi.getText() + "\n" + recived_data);
-                    }
-                }
-                catch (NullPointerException e) {
-                    e.printStackTrace();
-                }
-            }
         }else if (isServer()){
-
-            /**in case the pc acts as a server this function will listen to anything the client will write, if something is added, this will write it on the chatbox**/
-            String recived_data = server.data_recive;
-
-            while (true){
-                try {
-                    System.out.println("dato dopo " + server.data_recive);
-                    if(!server.data_recive.equals(recived_data)){
-                        System.out.println("dentro   " + recived_data);
-                        recived_data = server.data_recive;
-                        areaMessaggi.setText(areaMessaggi.getText() + "\n" + recived_data);
-                    }
-                }
-                catch (NullPointerException e) {
-                    e.printStackTrace();
-                }
-            }
+            /**in case the pc acts as a server this function will listen to anythig the client will write,
+             * if something is added, this will write it on the chatbox**/
+            server_is_listening();
         }
     }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
     }
+
     /**this method used in the method Start message is used to check if the pc acts as a client**/
     public boolean isClient(){
         if (Integer.parseInt(porta_client.getValue().toString()) > 0 && !ip_clientTextField.getText().isEmpty() && porta_server.getValue().equals(0))
